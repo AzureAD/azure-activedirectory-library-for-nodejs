b506e3e0896f0fea619c0068a0d0437141059e55
Rich Randall <rrandall@microsoft.com>
2014-12-12 17:37:17 -0800

    Update basic self-signed-jwt test to actually check that the returned jwt matches what was expected given static inputs.

--- test/self-signed-jwt.js
+++ test/self-signed-jwt.js
@@ -28,17 +28,25 @@ var util = require('./util/util');
 var cp = util.commonParameters;
 var testRequire = util.testRequire;
 
-var SelfSignedJwt = testRequire('self-signed-jwt').SelfSignedJwt;
+var SelfSignedJwt = testRequire('self-signed-jwt');
 
+var assert = require('assert');
+var sinon = require('sinon');
+
+var testNowDate = new Date(1418433646179);
+var testJwtId = '09841beb-a2c2-4777-a347-34ef055238a8';
+var expectedJwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IndWM3FobGF0MzJlLWdESFlYcjNjV3RiRU51RSJ9.eyJhdWQiOiJodHRwczovL2xvZ2luLndpbmRvd3MubmV0L25hdHVyYWxjYXVzZXMuY29tL29hdXRoMi90b2tlbiIsImlzcyI6ImQ2ODM1NzEzLWI3NDUtNDhkMS1iYjYyLTdhODI0ODQ3N2QzNSIsInN1YiI6ImQ2ODM1NzEzLWI3NDUtNDhkMS1iYjYyLTdhODI0ODQ3N2QzNSIsIm5iZiI6MTQxODQzMzY0NiwiZXhwIjoxNDE4NDM0MjQ2LCJqdGkiOiIwOTg0MWJlYi1hMmMyLTQ3NzctYTM0Ny0zNGVmMDU1MjM4YTgifQ.AS3jyf9nUqBPeEFKccYA2NfSOSjDoWGW_QTj7Jqjbwpmp8jnQRkJ1Q9QrWLBIspesUVtctiKZQAl_BMochF_4yopY_JbYkPKEVvpbTojtwjKgTpVF175NUjXibUNCijx1BXRxEHJUbVJqzVSWBFtRCbXVBPg_ODqC0JJWutynnwMDec93gGOdWGi8AfRwj855zP41aDZGhQVFiOn3apzN4yfhOGoEeTbG4_6921Tkducz2jWpfVTxIS4yIOKCa97J6XInIlP1iW8XAsnGnTevanj8ubfCtYNRcCOrzq_qZstD6tSDqhQjJlTj5B0zlVvMjTT6oDTAOjzL4TuruENEg';
+var testAuthority = {tokenEndpoint:'https://login.windows.net/naturalcauses.com/oauth2/token'};
+var testClientId = 'd6835713-b745-48d1-bb62-7a8248477d35';
 
 suite('self-signed-jwt', function() {
   test('create-jwt', function(done) {
-    var ssjwt = new SelfSignedJwt(cp.callContext, cp.authority, cp.clienId);
+    var ssjwt = new SelfSignedJwt(cp.callContext, testAuthority, testClientId);
+    sinon.stub(ssjwt, '_getDateNow').returns(testNowDate);
+    sinon.stub(ssjwt, '_getNewJwtId').returns(testJwtId);
     var jwt = ssjwt.create(util.getSelfSignedCert(), cp.certHash);
-    var err;
-    if (!jwt) {
-      err = new Error('Returned empty jwt');
-    }
-    done(err);
+    assert(jwt, 'No JWT generated');
+    assert(jwt === expectedJwt, 'Generated JWT does not match expected: ' + jwt);
+    done();
   });
 });
