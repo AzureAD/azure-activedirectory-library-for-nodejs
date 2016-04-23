e2fc5b1f8a6b492fb2b83e69cdf555bf8b258acb
abhishek58g <abgun@microsoft.com>
2016-04-22 17:28:53 -0700

    STRIKE :- Making the code CredScan clean
    STRIKE :- Making the code CredScan clean

--- test/util/util.js
+++ test/util/util.js
@@ -96,7 +96,7 @@ parameters.clientSecret = 'clientSecret*&^(?&';
 parameters.resource = '00000002-0000-0000-c000-000000000000';
 parameters.evoEndpoint = 'https://login.windows.net';
 parameters.username = 'rrandall@' + parameters.tenant;
-parameters.password = 'Atestpass!@#$';
+parameters.password = '<password>';
 parameters.authorityHosts = {
   global : 'login.windows.net',
   china : 'login.chinacloudapi.cn',
@@ -144,9 +144,9 @@ parameters.AssertionFile = __dirname + '/../wstrust/common.base64.encoded.assert
 parameters.logContext = { correlationId : 'test-correlation-id-123456789' };
 parameters.callContext = { _logContext : parameters.logContext };
 
-// 
-// a placeholder comment explaining why we have a fake cert here
-// 
+// This is a dummy RSA private cert used for testing purpose.It does not represent valid credential.
+// privatePem variable is a fake certificate in the form of a string.
+// Hence the following message is added to suppress CredScan warning.
 //[SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine")]
 util.getSelfSignedCert = function() {
   var privatePem = '-----BEGIN RSA PRIVATE KEY-----\n' +
